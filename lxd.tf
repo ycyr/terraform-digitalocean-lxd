b90bedc6d873547a1a1286dbc21aa031fcf2912f
ycyr <>
2017-10-02 23:59:14 -0400

    add firewall cleanning providers

--- lxd.tf
+++ lxd.tf
@@ -1,24 +1,16 @@
-provider "digitalocean" {
-  token = "${var.do_token}"
-  version = "~> v0.1.2"
-}
 
-provider "random" {
-  version = "~> v1.0.0"
-}
-
-
-/* resource "digitalocean_tag" "lxd" {
+ resource "digitalocean_tag" "lxd" {
   name = "lxd"
 }
 
-*/
+
 data "template_file" "userdata_web" {
   template = "${file("script.sh.tpl")}"
   depends_on = ["random_string.password"]
 
   vars = {
     lxd_password  = "${random_string.password.result}"
+    size = "${digitalocean_volume.lxd-volume.size}"
   }
 }
 
@@ -26,16 +18,24 @@ data "template_file" "userdata_web" {
 resource "digitalocean_droplet" "lxd" {
    image              = "${var.ubuntu}"
    region             = "${var.region}"
-   name = "lxd-1"
-   size = "2gb"
-#   size = "512mb"
-   ssh_keys = ["${var.ssh_key_ID}", "${var.ssh_key_ID2}"]
- #  tags   = ["${digitalocean_tag.lxd.id}"]
+   name = "lxd"
+   size = "${var.size_vm}"
+   #ssh_keys = ["${var.ssh_key_ID}", "${var.ssh_key_ID2}"]
+   ssh_keys = ["${var.ssh_key_ID}"]
+   tags   = ["${digitalocean_tag.lxd.id}"]
    user_data = "${data.template_file.userdata_web.rendered}"
-  provisioner "local-exec" {
-    command = "echo ${digitalocean_droplet.lxd.ipv4_address} > publicip.txt"
-  }
+    volume_ids = ["${digitalocean_volume.lxd-volume.id}"]
+   provisioner "local-exec" {
+       command = "echo ${digitalocean_droplet.lxd.ipv4_address} > publicip.txt"
+    }
 
+}
+
+resource "digitalocean_volume" "lxd-volume" {
+  region      = "${var.region}"
+  name        = "lxd-volume"
+  size        = "${var.size_volume}"
+  description = "lxd volume"
 }
 
 resource "random_string" "password" {
@@ -46,3 +46,44 @@ resource "random_string" "password" {
   }
 }
 
+resource "digitalocean_firewall" "lxd" {
+  name = "only-from-home"
+
+  droplet_ids = ["${digitalocean_droplet.lxd.id}"]
+
+  inbound_rule = [
+    {
+      protocol           = "tcp"
+      port_range         = "22"
+      source_addresses   = ["0.0.0.0/0", "::/0"]
+    },
+    {
+      protocol           = "tcp"
+      port_range         = "80"
+      source_addresses   = ["0.0.0.0/0", "::/0"]
+    },
+    {
+      protocol           = "tcp"
+      port_range         = "443"
+      source_addresses   = ["0.0.0.0/0", "::/0"]
+    },
+  ]
+   
+ outbound_rule = [
+   {
+    protocol                = "udp"
+    port_range              = "all"
+    destination_addresses   = ["0.0.0.0/0", "::/0"]
+  },
+   {
+    protocol                = "tcp"
+    port_range              = "all"
+    destination_addresses   = ["0.0.0.0/0", "::/0"]
+  },
+   {
+    protocol                = "icmp"
+    destination_addresses   = ["0.0.0.0/0", "::/0"]
+  }
+]
+  
+}
