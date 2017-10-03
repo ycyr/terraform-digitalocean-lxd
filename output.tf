35b69796ab1ad2c23867e21a8208ee1e00072aca
ycyr <>
2017-10-02 23:59:38 -0400

    remove output password

--- output.tf
+++ output.tf
@@ -1,10 +1,3 @@
 output "public_address" {
   value = ["${digitalocean_droplet.lxd.*.ipv4_address}"]
 }
-
-output "lxd_password" { 
-  value = ["${random_string.password.result}"]
-  sensitive = true
-} 
-
-
